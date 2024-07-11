789fb84571b1446469d707c0e7808397bcfc0b61
Brett Smith <brett03112@gmail.com>
2024-07-11 17:20:40 -0400

    initial commit

---
+++ chap08/WorkingWithNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;  // For BigInteger
+
+
+#region BigIntegers
+const int width = 40;
+
+WriteLine($"ulong.MaxValue vs. a 30-digit BigInteger:");
+WriteLine(new string('-', width));
+
+ulong big = ulong.MaxValue;
+WriteLine($"{big,width:N0}");
+
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+WriteLine($"{bigger,width:N0}\n");
+
+#endregion
+
+#region Complex Numbers
+
+
+#endregion
